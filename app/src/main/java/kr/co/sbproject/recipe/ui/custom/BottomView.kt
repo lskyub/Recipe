d91d2890736398e306667bd8f381f9bbe27d30a3
sgim <sgim@openit.co.kr>
2020-07-06 09:17:28 +0900

    일부 코드 제거

---
+++ app/src/main/java/kr/co/sbproject/recipe/ui/custom/BottomView.kt
@@ -0,0 +1,14 @@
+package kr.co.sbproject.recipe.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BottomView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : BottomNavigationView(context, attrs, defStyleAttr) {
+
+
+
+
+}
